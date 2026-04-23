c3f310d9e12ad2444082ca5f17fe5e72a93fd56a
alejandro1611 <cruzimanol944@gmail.com>
2026-04-23 17:46:58 -0300

    Hola mundo.js-Cruz Alejandro Imanol

---
+++ holaJSCruzAlejandro.js
@@ -0,0 +1,19 @@
+//NO USAR
+var nombre = "Alejandro"; 
+// trabajar con let y const 
+let apellido = "Cruz";
+//const TEXTO = "Hola Mundo, segun ";
+const TEXTO = `Hola Mundo, segun ${nombre} ${apellido}`;
+
+//console.log("Hola Mundo, segun "+nombre+apellido);
+//console.log(TEXTO+nombre+apellido);
+console.log(TEXTO)//NO USAR
+var nombre = "Alejandro"; 
+// trabajar con let y const 
+let apellido = "Cruz";
+//const TEXTO = "Hola Mundo, segun ";
+const TEXTO = `Hola Mundo, segun ${nombre} ${apellido}`;
+
+//console.log("Hola Mundo, segun "+nombre+apellido);
+//console.log(TEXTO+nombre+apellido);
+console.log(TEXTO)
