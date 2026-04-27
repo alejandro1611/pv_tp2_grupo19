4be22885bfa1c13f8a1859ba5ce42dc469a4ab16
alejandro1611 <cruzimanol944@gmail.com>
2026-04-27 16:47:46 -0300

    correcion del archivo holaJSCruzAlejandro.js Cruz Aleja ndro

--- holaJSCruzAlejandro.js
+++ holaJSCruzAlejandro.js
@@ -7,13 +7,4 @@ const TEXTO = `Hola Mundo, segun ${nombre} ${apellido}`;
 
 //console.log("Hola Mundo, segun "+nombre+apellido);
 //console.log(TEXTO+nombre+apellido);
-console.log(TEXTO)//NO USAR
-var nombre = "Alejandro"; 
-// trabajar con let y const 
-let apellido = "Cruz";
-//const TEXTO = "Hola Mundo, segun ";
-const TEXTO = `Hola Mundo, segun ${nombre} ${apellido}`;
-
-//console.log("Hola Mundo, segun "+nombre+apellido);
-//console.log(TEXTO+nombre+apellido);
-console.log(TEXTO)
+console.log(TEXTO);
