7a8897abd1b4554cb738122d650a4793fa6d44e0
Roberto Moraña <robertoalexmoranacastillo@gmail.com>
2026-04-29 21:13:53 -0300

    Añadidos ejercicios 8 y 14, y corrección de nombres de archivos. MorañaRoberto

---
+++ src/js/ejercicio1.js
@@ -0,0 +1,2 @@
+const integrantes = "Cruz Alejandro, Farfan Facundo, Gonzalez Lopez Alfredo Ezequiel, Mauricio Virazate, Moraña Roberto, Rita Ontiveros";
+console.log(`Somos ${integrantes} y estamos aprendiendo JavaScript.`);
