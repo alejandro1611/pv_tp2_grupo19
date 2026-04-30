76b5a7dc1bcdd79776f5a79bb19cb8da4a9bd7d3
Alfredo Ezequiel Gonzalez Lopez <eg156765@gmail.com>
2026-04-30 19:31:46 -0300

    desarollo ejercicio 23 - Alfredo Ezequiel Gonzalez Lopez

---
+++ src/js/ejercicio23.js
@@ -0,0 +1,14 @@
+import { mostrarTexto, cambiarColor } from "/src/services/serviceEjercicio23.js";
+
+const inputTexto = document.getElementById("textoInput");
+const resultado = document.getElementById("resultado");
+
+inputTexto.addEventListener("input", () => {
+
+    const texto = inputTexto.value;
+
+    mostrarTexto(resultado, texto);
+
+    cambiarColor(resultado, texto);
+
+});
